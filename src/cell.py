fc12d62f0c69503928583f25eb1c2ae6d6de94d6
Phillip Wood <Phillipwood2023@outlook.com>
2025-04-19 18:08:34 -0400

    Drawing move line from center of cell to another cell. Also, added edge case for cell's that do not touch(having a gap between them)

--- src/cell.py
+++ src/cell.py
@@ -8,19 +8,31 @@ class Cell():
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = top_left.x
-        self.__y1 = top_left.y
-        self.__x2 = bottom_right.x
-        self.__y2 = bottom_right.y
-        self.__win = window
+        self._x1 = top_left.x
+        self._y1 = top_left.y
+        self._x2 = bottom_right.x
+        self._y2 = bottom_right.y
+        self._win = window
 
     def draw(self, top_left: Vector2 = None, bottom_left: Vector2 = None):
         
         if self.has_left_wall:
-            self.__win.draw_line(Line(Vector2(self.__x1, self.__y1), Vector2(self.__x1, self.__y2)), "black")
+            self._win.draw_line(Line(Vector2(self._x1, self._y1), Vector2(self._x1, self._y2)), "black")
         if self.has_right_wall:
-             self.__win.draw_line(Line(Vector2(self.__x2, self.__y1), Vector2(self.__x2, self.__y2)), "black")
+             self._win.draw_line(Line(Vector2(self._x2, self._y1), Vector2(self._x2, self._y2)), "black")
         if self.has_top_wall:
-             self.__win.draw_line(Line(Vector2(self.__x1, self.__y1), Vector2(self.__x2, self.__y1)), "black")
+             self._win.draw_line(Line(Vector2(self._x1, self._y1), Vector2(self._x2, self._y1)), "black")
         if self.has_bottom_wall:
-             self.__win.draw_line(Line(Vector2(self.__x1, self.__y2), Vector2(self.__x2, self.__y2)), "black")
+             self._win.draw_line(Line(Vector2(self._x1, self._y2), Vector2(self._x2, self._y2)), "black")
+    
+    def draw_move(self, to_cell, undo=False):
+        color = "red" if not undo else "gray"
+        start = Vector2(
+            abs(self._x1 - self._x2),
+            abs(self._y1 - self._y2)
+        )
+        end = Vector2(
+            abs((to_cell._x1 - to_cell._x2)) + abs(self._x2 - self._x1) + abs(self._x2 - to_cell._x1),
+            abs((to_cell._y1 - to_cell._y2))
+        )
+        self._win.draw_line(Line(start, end), color)
--- src/main.py
+++ src/main.py
@@ -7,20 +7,14 @@ def main():
 
     win = Window(1920, 1080)
 
-    #cell = Cell(Vector2(100, 100), Vector2(300, 300), win)
-    # cell.has_right_wall = False
-    # cell2 = Cell(Vector2(300, 300), Vector2(500, 500), win)
-    # cell2.has_top_wall = False
-    cell3 = Cell(Vector2(500, 250), Vector2(700, 200), win)
-    # cell3.has_bottom_wall = False
-    cell4 = Cell(Vector2(400, 100), Vector2(500, 150), win)
-    cell5 = Cell(Vector2(2, 2), Vector2(550, 750), win)
-    cell5.has_right_wall = False
-    #cell.draw()
-    # cell2.draw()
-    cell3.draw()
-    cell4.draw()
-    cell5.draw()
+    cell = Cell(Vector2(100, 100), Vector2(300, 300), win)
+    cell.has_right_wall = False
+    cell2 = Cell(Vector2(400, 100), Vector2(600, 300), win)
+    cell2.has_left_wall = False
+    cell.draw()
+    cell2.draw()
+    cell.draw_move(cell2)
+
 
 
 
